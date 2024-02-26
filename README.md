b5a7149115db808f59d950dbf08423b1f4d7f9c3
ElStevenn <sfukinguay@gmail.com>
2024-02-26 22:37:58 +0100

    Libraries, SDKs and file structure it's done

---
+++ README.md
@@ -0,0 +1,4 @@
+
+## OVERVIEW
+
+ - A short Flask application to make the oauth 2.0 Authentication Server 
--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask, request
+from flask import Flask, request, redirect
+from routes import oauth2_authorization
+import os
 
 app = Flask(__name__)
-
+"""
+Documentation:
+https://developers.google.com/identity/protocols/oauth2/web-server#python_2
+https://developers.google.com/identity/protocols/oauth2/web-server
+"""
 
 @app.route("/")
 def root():
     return "<h1>OAUTH 2 authentication</h1>"
 
 
+@app.route("/oauth_autentication")
+def oauth_authentication():
+    return redirect(oauth2_authorization.authorization_url)
+
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
